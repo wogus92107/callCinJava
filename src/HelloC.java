4a783a0aedbdea3ca08e325b08a5e5fabe300ee0
wogus92107 <wogus92107@gmail.com>
2019-02-01 14:45:08 +0900

    ledtest

--- src/HelloC.java
+++ src/HelloC.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HelloC {
@@ -61,7 +59,7 @@ public class HelloC {
 
         public static void main(String args[]) {
 
-            try {
+     /*       try {
                 String s;
                 Process p;
                 ProcessBuilder pb = new ProcessBuilder();
@@ -76,7 +74,7 @@ public class HelloC {
                 OutputStream os = p.getOutputStream();
                 int data = is.read();
                 while(data != -1){
-                    os.
+
                     os.write(data);
                     os.flush();
                 }
@@ -89,7 +87,82 @@ public class HelloC {
                 System.out.println ("exit: " + p.exitValue());
               //  p.destroy();
                p.wait(100);
-            } catch (Exception e) {}
+            } catch (Exception e) {}*/
+            try{
+
+                ArrayList<String> commandlist = new ArrayList<>();
+                commandlist.add("echo");
+                commandlist.add("hello");
+
+
+
+
+            Scanner scan = new Scanner(System.in);
+            ProcessBuilder builder = new ProcessBuilder(commandlist);
+            builder.inheritIO();
+            builder.redirectErrorStream(true);
+
+
+
+
+            Process process = builder.start();
+
+
+            OutputStream stdin = process.getOutputStream ();
+
+
+            InputStream stderr = process.getErrorStream ();
+            InputStream stdout = process.getInputStream ();
+
+
+            BufferedReader reader = new BufferedReader (new InputStreamReader(stdout));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+
+                String line = reader.readLine();
+                while (line != null && ! line.trim().equals("--EOF--")) {
+                    System.out.println ("Stdout: " + line);
+                    line = reader.readLine();
+                }
+
+          line = reader.readLine();
+                while (line != null) {
+                    System.out.println ("Stdout: " + line);
+                    line = reader.readLine();
+                }
+
+               /* System.out.println(builder.directory().getName());*/
+                String input = scan.nextLine();
+
+                input += "\n";
+                writer.write(input);
+                writer.flush();
+
+                input = scan.nextLine();
+                input += "\n";
+                writer.write(input);
+                writer.flush();
+
+            while (scan.hasNext()) {
+             input = scan.nextLine();
+                if (input.trim().equals("exit")) {
+                    // Putting 'exit' amongst the echo --EOF--s below doesn't work.
+                    writer.write("exit\n");
+                } else {
+                    writer.write("((" + input + ") && echo --EOF--) || echo --EOF--\n");
+                }
+                writer.flush();
+
+                line = reader.readLine();
+                while (line != null && ! line.trim().equals("--EOF--")) {
+                    System.out.println ("Stdout: " + line);
+                    line = reader.readLine();
+                }
+                if (line == null) {
+                    break;
+                }
+            }
+            }catch(Exception ex){}
         }
 
 
